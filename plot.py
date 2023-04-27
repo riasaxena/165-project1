3fc469325c00d287dbe030f5ad0a351b07e76334
Ria Saxena <rias1@circinus-34.ics.uci.edu>
2023-04-27 14:40:19 -0700

    update plot.py

--- plot.py
+++ plot.py
@@ -2,11 +2,19 @@ from itertools import permutations
 import matplotlib as plt
 
 from pathlib import Path
-from benchmark import PermutationType
+# from benchmark import PermutationType
 from collections import defaultdict
 
 import numpy as np
 import csv
+from enum import Enum
+
+
+class PermutationType(Enum):
+    UNIFORMLY_DISTRIBUTED = 'random'
+    REVERSE_SORTED = 'reverse'
+
+DATA_DIRECTORY = Path('data')
 
 def get_data_path(permutation:PermutationType, algorithm_name: str) -> Path:
     directory = DATA_DIRECTORY/algorithm_name
@@ -16,6 +24,7 @@ def get_data_path(permutation:PermutationType, algorithm_name: str) -> Path:
 
 def load_data(algorithm_name: str, permutation:PermutationType):
     path = get_data_path(permutation, algorithm_name)
+    print(path)
 
     data = defaultdict(list)
     with path.open() as csvfile:
@@ -25,5 +34,21 @@ def load_data(algorithm_name: str, permutation:PermutationType):
             data[int(row[0])].append(int(row[1]))
         
     return data
+def load_avg_data(algorithm_name: str, permutation: PermutationType):
+    data = load_data(algorithm.name, permutation)
+    sizes = avg_times = list(), list()
 
-print(load_data('insertion_sort', PermutationType.UNIFORMLY_DISTRIBUTED))
+    for size, elapsed_time in sorted(data.items()):
+        size.append(size)
+        avg_times.append(sum(elapsed_time)/ len (elapsed_time))
+
+    return sizes, avg_times
+def add_to_plot(algorithm_name: str, permutation: PermutationType):
+    sizes, avg_times = load_avg_data
+    plt.plot(sizes, avg_times)
+
+
+if __name__ == '__main___':
+    add_to_plot('insertion_sort', PermutationType.UNIFORMLY_DISTRIBUTED)
+    plt.show()
+# print(load_data('insertion_sort', PermutationType.UNIFORMLY_DISTRIBUTED))
