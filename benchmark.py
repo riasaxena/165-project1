b6f7cd2b238301c08eb1604f3acf9405d2da7864
Ria Saxena <rias1@circinus-17.ics.uci.edu>
2023-04-27 23:33:31 -0700

    update benchmark to create data directory

--- benchmark.py
+++ benchmark.py
@@ -1,53 +1,67 @@
 import argparse
+from enum import Enum
 import requirements
 import random
-from enum import Enum
-from re import S
 import time
+from pathlib import Path
+import csv
+
+
+DATA_DIRECTORY = Path('data')
+
 class PermutationType(Enum):
     UNIFORMLY_DISTRIBUTED = 'random'
     REVERSE_SORTED = 'reverse'
 
 SORTING_ALGORITHMS = {
     'insertion_sort' : requirements.insertion_sort,
+    'merge_sort' : requirements.merge_sort,
+    'shell_sort1': requirements.shell_sort1,
+    'shell_sort2': requirements.shell_sort2,
+    'shell_sort3': requirements.shell_sort3,
+    'shell_sort4': requirements.shell_sort4,
+    'hybrid_sort1': requirements.hybrid_sort1,
+    'hybrid_sort2': requirements.hybrid_sort2,
+    'hybrid_sort3': requirements.hybrid_sort3,
 }
-parser = argparse.ArgumentParser(description='Benchmark several algorithms')
-parser.add_argument('size', metavar = 'S', type = int, help= 'the input size for the sorting algorithm')
 
+parser = argparse.ArgumentParser(description = "Benchmark several algorithms. ")
+
+parser.add_argument('size', metavar = 'S', help = 'the input size for the sorting algorithm', type = int)
 parser.add_argument('--permutation', choices = [e.value for e in PermutationType ], help = 'the input permutation type to be used', default = 'random')
-
 parser.add_argument ('--algorithm', dest='algorithm_name', choices = SORTING_ALGORITHMS.keys(), help = 'the sorting algorithm to use', required = True)
-
-args = parser.parse_args()
-args.permutation = PermutationType(args.permutation)
-args.algorithm = SORTING_ALGORITHMS[args.algorithm_name]
-
-# print(args.size, type(args.size))
-# print(args.permutation, type(args.permutation))
-# print(args.algorithm, type(args.algorithm))
-
 
 def generate_list(permutation: PermutationType, size: int) -> list[int]:
     nums = list (range (size))
-
     match permutation:
         case PermutationType.UNIFORMLY_DISTRIBUTED:
             random.shuffle(nums)
         case PermutationType.REVERSE_SORTED:
             nums.sort(reverse=True)
     return nums
+def get_data_path(permutation: PermutationType, algorithm_name: str) -> Path:
+    directory = DATA_DIRECTORY/ algorithm_name
+    directory.mkdir(parents = True, exist_ok = True)
+
+    return(directory / permutation.value).with_suffix('.csv')
+
+def save_data(size: int, elapsed_time: int):
+    path = get_data_path(args.permutation, args.algorithm_name)
+    with path.open('a') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([size, elapsed_time])
 
 def benchmark(permutation: PermutationType, algorithm: 'function',  size: int):
     nums =  generate_list(permutation, size)
-    start_time = time.process_time()
+    start_time = time.process_time_ns()
     algorithm(nums)
-    end_time = time.process_time()
+    end_time = time.process_time_ns()
     elapsed_time = end_time - start_time
-    print(nums, elapsed_time)
-
-    
-
-
+    # print(nums, elapsed_time)
+    save_data(size, elapsed_time)
 
 if __name__ == '__main__':
+    args = parser.parse_args()
+    args.permutation = PermutationType(args.permutation)
+    args.algorithm = SORTING_ALGORITHMS[args.algorithm_name]
     benchmark(args.permutation, args.algorithm, args.size)
--- plot.py
+++ plot.py
@@ -2,7 +2,6 @@ from itertools import permutations
 import matplotlib as plt
 
 from pathlib import Path
-# from benchmark import PermutationType
 from collections import defaultdict
 
 import numpy as np
@@ -18,14 +17,12 @@ DATA_DIRECTORY = Path('data')
 
 def get_data_path(permutation:PermutationType, algorithm_name: str) -> Path:
     directory = DATA_DIRECTORY/algorithm_name
-    directory.mkdir(parents = True)
+    directory.mkdir(parents = True, exist_ok = True)
 
     return (directory / permutation.value).with_suffix('.csv')
 
 def load_data(algorithm_name: str, permutation:PermutationType):
     path = get_data_path(permutation, algorithm_name)
-    print(path)
-
     data = defaultdict(list)
     with path.open() as csvfile:
         reader = csv.reader(csvfile)
@@ -35,20 +32,21 @@ def load_data(algorithm_name: str, permutation:PermutationType):
         
     return data
 def load_avg_data(algorithm_name: str, permutation: PermutationType):
-    data = load_data(algorithm.name, permutation)
-    sizes = avg_times = list(), list()
+    data = load_data(algorithm_name, permutation)
+    sizes, avg_times = list(), list()
 
     for size, elapsed_time in sorted(data.items()):
-        size.append(size)
+        sizes.append(size)
         avg_times.append(sum(elapsed_time)/ len (elapsed_time))
 
     return sizes, avg_times
 def add_to_plot(algorithm_name: str, permutation: PermutationType):
-    sizes, avg_times = load_avg_data
-    plt.plot(sizes, avg_times)
+    sizes, avg_times = load_avg_data(algorithm_name, permutation)
+    plt.loglog(sizes, avg_times)
 
 
 if __name__ == '__main___':
     add_to_plot('insertion_sort', PermutationType.UNIFORMLY_DISTRIBUTED)
     plt.show()
 # print(load_data('insertion_sort', PermutationType.UNIFORMLY_DISTRIBUTED))
+# print(load_avg_data('insertion_sort', PermutationType.UNIFORMLY_DISTRIBUTED))
